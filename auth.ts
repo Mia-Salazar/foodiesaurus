88c9badb851b830f8ee8141abc4c2b34183ceaad
Mia-Salazar <maria.sgr@gmail.com>
2026-09-05 18:22:07 +0200

    hotfix: fix google auth

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ import { prisma } from "./server/infraestructure/database/prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
 
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
 
   providers: [
     Google({
